83c9c598b188282027312876a8e04a0a2681f3c7
HackBro <167735173+Lackybot@users.noreply.github.com>
2024-11-26 23:25:17 -0500

    Create installerchecks.py
    see if u have some sorta error

---
+++ installerchecks.py
@@ -0,0 +1,6 @@
+try:
+    import robloxpy
+    print("Module 'robloxpy' imported successfully.")
+    print("RobloxPY version = 0.2.21")
+except ImportError:
+    print("Module 'robloxpy' is not installed. Please install it using 'pip3 install robloxpy'.")
