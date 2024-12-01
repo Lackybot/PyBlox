8ac065c12bf01caf2714fe42af2ae399ffb0520d
Thankful Python <167735173+pydev11@users.noreply.github.com>
2024-11-30 19:22:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import robloxpy
 import requests
 import json
+import pyautogui
+
+print("Started Main.py")
+
+try:
+    import pyautogui
+except ImportError:
+    print("Please install pyautogui by running: pip3 install pyautogui")
